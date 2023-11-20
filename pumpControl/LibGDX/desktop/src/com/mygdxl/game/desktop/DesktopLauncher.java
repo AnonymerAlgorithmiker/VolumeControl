eec3d2d64b3b68343c1a94844ae34cb3147a3bbd
AnonymerAlgorithmiker <linus_diepold@web.de>
2023-11-20 17:25:59 +0100

    Add to new GitHub Profile

---
+++ pumpControl/LibGDX/desktop/src/com/mygdxl/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdxl.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdxl.game.VolumeControlV2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new VolumeControlV2(), config);
+	}
+}
